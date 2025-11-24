fdbcd23e407486caa26aa212e637a3800753c7e4
JoGei <62236820+JoGei@users.noreply.github.com>
2025-11-24 17:40:10 +0100

    ci: add tests/crc workflow

--- docker/setup_debian.sh
+++ docker/setup_debian.sh
@@ -13,6 +13,18 @@ setup_env() {
     apt install --no-install-recommends -y ${pkg}
   done
 }
+get_testenv_apt_deps() {
+  spike_apt_dep="device-tree-compiler libboost-regex-dev libboost-system-dev"
+  base_apt_dep="$(get_apt_deps)"
+  echo "${base_apt_dep} ${spike_apt_dep}"
+}
+setup_testenv() {
+  apt update
+  for pkg in "$(get_testenv_apt_deps)"
+  do
+    apt install --no-install-recommends -y ${pkg}
+  done
+}
 ########################################################################################################################
 # LLVM
 fetch_llvm() {
@@ -107,4 +119,56 @@ setup_compas() {
   ln -s "${compas_src_dir}" "compas-ft-riscv"
   cd "${_home_}"
 }
+########################################################################################################################
+# RISC-V GNU Toolchain
+fetch_rvgnu() {
+  rvgnu_dir="$1"
+  url="$2"
+  target_name="$3"
 
+  echo "[fetch] risc-v gnu tools"
+  wget "${url}" --output-document="${target_name}.tar.gz"
+  tar xf "${target_name}.tar.gz"
+  mv "${target_name}" "${rvgnu_dir}"
+  rm "${target_name}.tar.gz"
+
+  spike_ref="v1.1.0"
+  spike_url="https://github.com/riscv-software-src/riscv-isa-sim"
+  git clone --depth 1 --branch "${spike_ref}" ${spike_url}.git /spike-src
+
+  pk_ref="v1.0.0"
+  pk_url="https://github.com/riscv-software-src/riscv-pk"
+  git clone --depth 1 --branch "${pk_ref}" ${pk_url}.git /pk-src  
+}
+configure_rvgnu() {
+  _home_=${PWD}
+  rvgnu_dir="$1"
+
+  echo "[configure] risc-v gnu tools"
+  export RISCV="${rvgnu_dir}"
+  export PATH="${RISCV}/bin:${PATH}"
+
+  mkdir -p /spike-src/build && cd /spike-src/build
+  ../configure --prefix=${RISCV}
+
+  mkdir -p /pk-src/build && cd /pk-src/build
+  ../configure --prefix=$RISCV --host=riscv-none-elf --with-arch=rv64imafdc_zicsr_zifencei
+  make -j $(nproc)
+  make install
+
+  cd ${_home_}
+}
+build_rvgnu() {
+  echo "[build] risc-v gnu tools"
+  make -C /pk-src/build -j $(nproc)
+  make -C /spike-src/build -j $(nproc)
+}
+install_rvgnu() {
+  echo "[install] risc-v gnu tools"
+  make -C /pk-src/build  install
+  make -C /spike-src/build  install
+}
+cleanup_rvgnu() {
+  echo "[clean-up] risc-v gnu tools"
+  rm -rf "/spike-src/" "/pk-src/"
+}
